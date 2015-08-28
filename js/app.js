2e62a4e70b9e6de9c8f4259e59bce2f3c1a2edcf
jdf5196 <jfrancona87@gmail.com>
2015-08-27 21:45:36 -0400

    Overlay works

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 $(function(){
-
+	$("#siteHack").click(function(){
+		$("#overlay").addClass("sitehack").removeClass("blank")
+		$('.fadeout').html('Close');
+		$(".overlayText").html("First Thinkful project. Used the Chrome developer tools to change the content of the New York Times front page.");
+		$("#overlay").fadeIn(500);
+	});
+	$("#siteClone").click(function(){
+		$("#overlay").addClass("siteclone").removeClass("blank")
+		$('.fadeout').html('Close');
+		$(".overlayText").html("Second Thinkful project. Used HTML and CSS to develope a clone of a startup landing page.");
+		$("#overlay").fadeIn(500);
+	});
+	$(".fadeout").click(function(){
+		$("#overlay").fadeOut(500);
+		$('.fadeout').html('');
+		$(".overlayText").html("");
+		$("#overlay").addClass("blank")
+		$("#overlay").removeClass("sitehack")
+		$("#overlay").removeClass("siteclone")
+	})
 
 });
